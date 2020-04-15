6b365fb48ef5367eefe25544a48881d3fc5d4e52
Patrick Lenger <patrick@core>
2020-04-15 12:57:58 +0200

    Add calibration

--- src/main.cpp
+++ src/main.cpp
@@ -8,6 +8,7 @@
 const byte INPUT_CHANGE_VALUE = 2;    // Arduino port D2
 const byte INPUT_NEXT = 3;            // Arduino port D3
 const byte OUTPUT_POWER = 4;          // Arduino port D4
+const byte OUTPUT_TOGGLE = 5;         // Arduino port D5
 const byte INPUT_VOLTAGE = 1;         // Arduino port A1
 
 // define modes
@@ -18,6 +19,7 @@ const byte MODE_RUNNING = 3;
 const byte MODE_FINISHED = 4;
 
 // other constants
+const byte DEBUG = 0; // 1: debug mode on, 0: debug mode off
 const byte MAX_PPM = 100;
 const byte MIN_PPM = 5;
 const unsigned int MAX_VOLUME_ML = 1000;
@@ -26,8 +28,8 @@ const byte STEP_PPM = 5;
 const unsigned int STEP_VOLUME = 50;
 const unsigned long INTERVAL_MEASURE_CURRENT_MILLIS = 10000;
 const byte CALCULATE_PPM_AFTER_NUMBER_CURRENT_MEASUREMENTS = 6;
-const unsigned int RESISTOR_MEASUREMENT_OHM = 1000;
-const float REF_VOLTAGE = 4.66;
+const unsigned int RESISTOR_MEASUREMENT_OHM = 10000;
+const float REF_VOLTAGE = 5.00;
 const char* PPM      = "ppm: ";
 const char* ML       = "ml:  ";
 const char* MINUTES  = "min: ";
@@ -43,6 +45,8 @@ byte numberCurrentMeasurements = 0;
 float ppmSum = 0;
 float mASum = 0;
 float minutesSum = 0.0;
+float calibrationMA = 0.0;
+byte outputOn = 0;
 
 // Display
 #define OLED_RESET 5
@@ -106,12 +110,36 @@ void printFinished() {
   display.display();
 }
 
-void start() {
+void printDebugCurrentVoltAndMA() {
+  float volt = measureVolt();
+  float mA = calculateMA(volt);
+  //float correctedMA = mA - ((REF_VOLTAGE / (1100.0 + 68000.0 + 10000.0) * 1000.0));
+  //float correctedMA = mA - calibrationMA;
+  display.clearDisplay();
+  display.setCursor(0,0);
+  String textMA = String(mA) + " mA";
+  String textVolt = String(volt) + " V";
+  //String textCorrectedMA = String(correctedMA) + " mA";
+  String textRunning = (mode == MODE_RUNNING) ? "ON" : "OFF";
+  display.println(textVolt);
+  display.println(textMA);
+  //display.println(textCorrectedMA);
+  display.println(textRunning);
+  display.display();
+}
+
+void switchOutputOn() {
   digitalWrite(OUTPUT_POWER, 1);
+  outputOn = 1;
+}
+
+void switchOutputOff() {
+  digitalWrite(OUTPUT_POWER, 0);
+  outputOn = 0;
 }
 
 void finish() {
-  digitalWrite(OUTPUT_POWER, 0);
+  switchOutputOff();
   mode = MODE_FINISHED;
 }
 
@@ -122,7 +150,7 @@ void handleButtonChangeValue() {
 
   int valNew = digitalRead(INPUT_CHANGE_VALUE);
   if (valNew != valButtonChangeValue) {
-    // button set state changed
+    // button set state chancalculateMAged
     valButtonChangeValue = valNew;
     delay(10); // entprellen
 
@@ -166,18 +194,26 @@ void handleButtonNext() {
       } else if (mode == MODE_ASKING_START) {
         printAskStart();
       } else if (mode == MODE_RUNNING) {
-        start();
+        switchOutputOn();
         printCurrentPpm();
       }
     }
   }
 }
 
-float measureMA() {
-  // measure voltage on A1 and calculate current in mA
+float measureVolt() {
+  // measure voltage on A1
   int analogVal = analogRead(INPUT_VOLTAGE);
   float volt = analogVal * (REF_VOLTAGE / 1024.0);
+  return volt;
+}
+
+float calculateMA(float volt) {
+  // calculate current in mA
   float mA = (volt / RESISTOR_MEASUREMENT_OHM) * 1000.0;
+  if (outputOn == 1) {
+    mA = mA - calibrationMA;
+  }
   Serial.println(String(volt) + " V");
   Serial.println(String(mA) + " mA");
   return mA;
@@ -195,6 +231,26 @@ void updatePpm() {
   minutesSum = minutesSum + minutes;
 }
 
+/* Measure the current for calibration */
+void calibrate() {
+  switchOutputOn();
+  float volt = measureVolt();
+  calibrationMA = calculateMA(volt);
+  switchOutputOff();
+}
+
+/* Show voltage and current for calibration reasons */
+void debug() {
+  if (mode != MODE_RUNNING) {
+    switchOutputOn();
+    mode = MODE_RUNNING;
+  } else {
+    finish();
+  }
+  printDebugCurrentVoltAndMA();
+  delay(5000);
+}
+
 void setup()
 {
   Serial.begin(9600);
@@ -202,33 +258,42 @@ void setup()
   pinMode(INPUT_CHANGE_VALUE, INPUT);
   pinMode(INPUT_NEXT, INPUT);
   pinMode(OUTPUT_POWER, OUTPUT);
+  pinMode(OUTPUT_TOGGLE, OUTPUT);
 
   printPpm(); // print initial ppm value
+
+  calibrate(); // measure calibration current
 }
 
 void loop()
 {
-  handleButtonChangeValue();
-  handleButtonNext();
+  if (DEBUG == 1) {
+    debug();
+  } else {
+    handleButtonChangeValue();
+    handleButtonNext();
+  
+    if (mode == MODE_RUNNING) {
+      // measure current, update ppm and refresh display
+      unsigned long currentMillis = millis();
 
-  if (mode == MODE_RUNNING) {
-    // measure current, update ppm and refresh display
-    unsigned long currentMillis = millis();
-    if ((currentMillis - lastMillisMeasuredCurrent) > INTERVAL_MEASURE_CURRENT_MILLIS) {
-      lastMillisMeasuredCurrent = currentMillis;
-      float mA = measureMA();
-      mASum = mASum + mA;
-      numberCurrentMeasurements++;
-
-      if (numberCurrentMeasurements == CALCULATE_PPM_AFTER_NUMBER_CURRENT_MEASUREMENTS) {
-        // end of interval of current measurement reached -> update ppm and check if target ppm reached
-        updatePpm();
-        printCurrentPpm();
-        numberCurrentMeasurements = 0;
-        mASum = 0;
-        if (ppmSum >= ppmTarget) {
-          finish();
-          printFinished();
+      if ((currentMillis - lastMillisMeasuredCurrent) > INTERVAL_MEASURE_CURRENT_MILLIS) {
+        lastMillisMeasuredCurrent = currentMillis;
+        float volt = measureVolt();
+        float mA = calculateMA(volt);
+        mASum = mASum + mA;
+        numberCurrentMeasurements++;
+  
+        if (numberCurrentMeasurements == CALCULATE_PPM_AFTER_NUMBER_CURRENT_MEASUREMENTS) {
+          // end of interval of current measurement reached -> update ppm and check if target ppm reached
+          updatePpm();
+          printCurrentPpm();
+          numberCurrentMeasurements = 0;
+          mASum = 0;
+          if (ppmSum >= ppmTarget) {
+            finish();
+            printFinished();
+          }
         }
       }
     }
